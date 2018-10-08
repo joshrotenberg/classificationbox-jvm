4290ff890f2b62ab08c1985c0d3149d414ca1dc2
josh rotenberg <josh.rotenberg@capitalone.com>
2018-10-08 10:39:02 -0700

    javadoc

--- src/main/java/io/machinebox/classificationbox/ClassificationBoxClient.java
+++ src/main/java/io/machinebox/classificationbox/ClassificationBoxClient.java
@@ -10,6 +10,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
 
+/**
+ * ClassificationBoxClient wraps a {@link ClassificationBoxService} suitable for interacting with a
+ * <a href="https://machinebox.io">Machine Box</a> <a href="https://machinebox.io/docs/classificationbox"> Classificationbox</a>
+ * instance.
+ */
 public class ClassificationBoxClient {
 
     private final ClassificationBoxService classificationBoxService;
@@ -18,10 +23,27 @@ public class ClassificationBoxClient {
         this.classificationBoxService = service;
     }
 
+    /**
+     * Access the actual service instance.
+     *
+     * @return the {@link ClassificationBoxService} service handle
+     */
     public ClassificationBoxService service() {
         return this.classificationBoxService;
     }
 
+    /**
+     * Get a default client pointing at localhost:8080 with no authentication.
+     *
+     * @return a ClassificationBoxClient with the default configuration
+     */
+    public static ClassificationBoxClient defaultClient() {
+        return ClassificationBoxClient.newBuilder().build();
+    }
+
+    /**
+     * A builder for configuring a {@link ClassificationBoxClient}.
+     */
     public static class Builder {
         public static final String DEFAULT_BASE_URL = "http://localhost:8080/";
 
@@ -37,11 +59,25 @@ public class ClassificationBoxClient {
             this.baseUrl = DEFAULT_BASE_URL;
         }
 
+        /**
+         * Set the baseUrl for your Classificationbox instance, i.e. "http://my.cb.org/"
+         *
+         * @param baseUrl a fully qualified top level url
+         * @return {@link ClassificationBoxClient.Builder}
+         */
         public Builder baseUrl(String baseUrl) {
             this.baseUrl = baseUrl;
             return this;
         }
 
+        /**
+         * Set the HTTP Basic Authentication username and password for your Classificationbox instance. Setting these will
+         * implicitly use authentication for all calls to the configured instance.
+         *
+         * @param username your Basic Authentication username
+         * @param password your Basic Authentication password
+         * @return {@link ClassificationBoxClient.Builder}
+         */
         public Builder withAuthentication(String username, String password) {
             this.okHttpClientBuilder.authenticator(new Authenticator() {
                 @Override
@@ -59,11 +95,22 @@ public class ClassificationBoxClient {
             return this;
         }
 
+        /**
+         * Add an OkHttpInterceptor to the underlying OkHttp client.
+         *
+         * @param interceptor an OkHttp interceptor
+         * @return {@link ClassificationBoxClient.Builder}
+         */
         public Builder addInterceptor(Interceptor interceptor) {
             this.okHttpClientBuilder.addInterceptor(interceptor);
             return this;
         }
 
+        /**
+         * Build the client.
+         *
+         * @return {@link ClassificationBoxClient}
+         */
         public ClassificationBoxClient build() {
 
             Gson gson = new GsonBuilder()
@@ -81,6 +128,11 @@ public class ClassificationBoxClient {
         }
     }
 
+    /**
+     * Get a new {@link ClassificationBoxClient.Builder}
+     *
+     * @return {@link ClassificationBoxClient.Builder}
+     */
     public static Builder newBuilder() {
         return new Builder();
     }
