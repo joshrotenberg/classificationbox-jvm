51e204cab73cf676c4becc58e51c6951e9c3feff
josh rotenberg <josh.rotenberg@capitalone.com>
2018-10-08 12:38:23 -0700

    docs for feature types and model options

--- src/main/java/io/machinebox/classificationbox/common/FeatureType.java
+++ src/main/java/io/machinebox/classificationbox/common/FeatureType.java
@@ -3,20 +3,79 @@ package io.machinebox.classificationbox.common;
 import com.google.gson.annotations.SerializedName;
 
 public enum FeatureType {
+    /**
+     * An integer or floating point number (e.g. likes, age, size)
+     */
     @SerializedName("number") NUMBER,
+
+    /**
+     * A blob of text that will broken down into tokens (e.g. body of an article)
+     */
     @SerializedName("test") TEXT,
+
+    /**
+     * A string value that will not be tokenized (e.g. cities, authors)
+     */
     @SerializedName("keyword") KEYWORD,
+
+    /**
+     * A comma separated list of keywords (e.g. visit history articleA,articleB,articleC,...)
+     */
     @SerializedName("list") LIST,
+
+    /**
+     * The URL to an image (which will be downloaded by Classificationbox)
+     */
     @SerializedName("image_url") IMAGE_URL,
+
+    /**
+     * A string containing the base64 encoded image data
+     */
     @SerializedName("image_base64") IMAGE_BASE64,
 
+
+    /**
+     * A blob of text in English
+     */
     @SerializedName("text_en") TEXT_EN,
+
+    /**
+     * A blob of text in Spanish
+     */
     @SerializedName("text_sp") TEXT_SP,
+
+    /**
+     * A blob of text in French
+     */
     @SerializedName("text_fr") TEXT_FR,
+
+    /**
+     * A blob of text in Russian
+     */
     @SerializedName("text_ru") TEXT_RU,
+
+    /**
+     * A blob of text in Swedish
+     */
     @SerializedName("text_sv") TEXT_SV,
+
+    /**
+     * A blob of text in Chinese
+     */
     @SerializedName("text_zh") TEXT_ZH,
+
+    /**
+     * A blob of text in German
+     */
     @SerializedName("text_ge") TEXT_GE,
+
+    /**
+     * A blob of text in Dutch
+     */
     @SerializedName("text_nl") TEXT_NL,
+
+    /**
+     * A blob of text in Portuguese
+     */
     @SerializedName("text_pt") TEXT_PT;
 }
--- src/main/java/io/machinebox/classificationbox/common/ModelOption.java
+++ src/main/java/io/machinebox/classificationbox/common/ModelOption.java
@@ -3,6 +3,13 @@ package io.machinebox.classificationbox.common;
 import com.google.gson.annotations.SerializedName;
 
 public enum ModelOption {
+    /**
+     * The number of word n-grams to generate from a body of text (default is 1)
+     */
     @SerializedName("ngrams") NGRAMS,
+
+    /**
+     * The distance between words from which to generate n-grams (default is 0, no skipping)
+     */
     @SerializedName("skipgrams") SKIPGRAMS;
 }
